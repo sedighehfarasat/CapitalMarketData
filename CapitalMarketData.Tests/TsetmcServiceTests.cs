9ee894e17768481776a78af0396d2d4919bdc605
Sedigheh <sd.farasat@gmail.com>
2023-07-16 19:00:44 +0330

    Add tests.

--- CapitalMarketData.Tests/TsetmcServiceTests.cs
+++ CapitalMarketData.Tests/TsetmcServiceTests.cs
@@ -21,9 +21,30 @@ public class TsetmcServiceTests
     }
 
     [Fact]
-    public async Task TsetmcService_GetPriceData()
+    public async Task GetInstitutionalIndividualData()
+    {
+        string insCode = "24651394045981418";
+        await TsetmcService.GetInstitutionalIndividualData(insCode);
+    }
+
+    [Fact]
+    public async Task GetPriceData()
     {
         string insCode = "24651394045981418";
         await TsetmcService.GetPriceData(insCode);
+    }
+
+    [Fact]
+    public async Task GetInstrumentState()
+    {
+        string insCode = "24651394045981418";
+        await TsetmcService.GetInstrumentState(insCode);
+    }
+
+    [Fact]
+    public async Task GetStaticThresholds()
+    {
+        string insCode = "24651394045981418";
+        await TsetmcService.GetStaticThresholds(insCode);
     }
 }
